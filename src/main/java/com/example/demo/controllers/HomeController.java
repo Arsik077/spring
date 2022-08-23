bd919f407112cc3c67c451c27e131cd71edfc40f
Arsen <arsik077@mail.ru>
2022-08-23 20:35:46 +0600

    Arailym's changes

--- src/main/java/com/example/demo/controllers/HomeController.java
+++ src/main/java/com/example/demo/controllers/HomeController.java
@@ -1,5 +1,9 @@
 package com.example.demo.controllers;
 
 public class HomeController {
-    private static String HELLO_WORLD = "Hello world!";
+    private static String MY_VARIABLE = "Hello world!";
+
+
+
+    private static String QWERT = "QWERT";
 }
